cee10bfb725cdc3ee8baa6889fe0b3a9e37ceca0
PaulRomanov <pavel.ramanau@gmail.com>
2023-10-31 13:42:33 +0400

    add  some components

---
+++ src/components/ContentApp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="exchange_history"></div>
+  <div class="wrapper_btns">
+    <div class="wrapper_filter"></div>
+    <div class="wrapper_action"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentApp",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.exchange_history {
+  width: 678px;
+  height: 570px;
+  margin: 25px 0 0 27px;
+  background-color: rgb(62, 1, 120);
+}
+.wrapper_btns {
+  width: 391px;
+  height: 570px;
+  margin: 25px 0 0 0;
+  background-color: salmon;
+}
+</style>
---
+++ src/components/MenuApp.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <img src="@/assets/img/logo.svg" alt="Иконка">
+    <ul>
+      <li v-for="item in menuItems" :key="item.id">
+        <img :src="require(`@/assets/img/${item.id}.svg`)" alt="Иконка">
+        <router-link :to="item.route" class="nav_item">{{ item.label }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MenuApp",
+  data() {
+  return {
+    menuItems: [
+      { id: 1, label: 'Wallet', route: '/' },
+      { id: 2, label: 'DEX', route: '/DEX' },
+      { id: 3, label: 'TX explorer', route: '/TXexplorer' },
+      { id: 4, label: 'Certificates', route: '/Certificates' },
+      { id: 5, label: 'Tokens', route: '/Tokens' },
+      { id: 6, label: 'VPN client', route: '/VPNclient' },
+      { id: 7, label: 'VPN service', route: '/VPNservice' },
+      { id: 8, label: 'Console', route: '/Console' },
+      { id: 9, label: 'Logs', route: '/Logs' },
+      { id: 10, label: 'Settings', route: '/Settings' },
+      { id: 11, label: 'dApps', route: '/dApps' },
+    ]
+  };
+},
+
+};
+</script>
+
+<style scoped>
+.nav_item{
+font-family: 'Quicksand';
+font-size: 13px;
+font-weight: 400;
+line-height: 16px;
+letter-spacing: 0px;
+text-align: left;
+color: #FFFFFF;
+}
+</style>
