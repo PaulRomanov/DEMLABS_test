876274ca335fa7c2b927a767123abb842c31028c
PaulRomanov <pavel.ramanau@gmail.com>
2023-10-31 15:10:29 +0400

    add components

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <div>404</div>
+    <div>Not found</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorPage",
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  font-size: 80px;
+}
+</style>
---
+++ src/components/StartComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">Выберите нужный пункт меню (Certificates)</div>
+</template>
+
+<script>
+export default {
+  name: "StartComponent",
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  font-size: 40px;
+}
+</style>
